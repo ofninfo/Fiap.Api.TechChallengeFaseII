cd16c633a07a82f5130083155baf43684d9ff088
Isaias Silva <ijsilva@apoioprodesp.sp.gov.br>
2024-01-27 20:37:34 -0300

    Alteração de "summary": "Chilly" para  "summary": "Chilly (Teste)". Adicionando a palavra teste no valor do summary

--- Fiap.Api.TechChallengeFaseII/Controllers/WeatherForecastController.cs
+++ Fiap.Api.TechChallengeFaseII/Controllers/WeatherForecastController.cs
@@ -30,11 +30,11 @@ namespace Fiap.Api.TechChallengeFaseII.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]+" (Teste)"
             })
             .ToArray();
         }
