97cfbd6582686a1425ed7dcbf74ff36c95c290ec
Isaias Silva <ijsilva@apoioprodesp.sp.gov.br>
2024-01-27 19:10:41 -0300

    Primeira alteração no código

--- Fiap.Api.TechChallengeFaseII/Controllers/WeatherForecastController.cs
+++ Fiap.Api.TechChallengeFaseII/Controllers/WeatherForecastController.cs
@@ -8,7 +8,16 @@ namespace Fiap.Api.TechChallengeFaseII.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing",
+            "Bracing", 
+            "Chilly", 
+            "Cool", 
+            "Mild", 
+            "Warm", 
+            "Balmy", 
+            "Hot", 
+            "Sweltering", 
+            "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
@@ -21,7 +30,7 @@ namespace Fiap.Api.TechChallengeFaseII.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 10).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
